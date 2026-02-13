0eda68ed608db24eaa6db942620722221f74d429
Jorge Diaz <joredudiaz@gmail.com>
2026-02-13 12:54:59 -0500

    refactor: remove unnecessary imports

--- src/main/java/com/mvc/example/controllers/ItemController.java
+++ src/main/java/com/mvc/example/controllers/ItemController.java
@@ -1,6 +1,5 @@
 package com.mvc.example.controllers;
 
-import com.mvc.example.models.Item;
 import com.mvc.example.models.ItemRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Controller
 @RequestMapping("/items")
